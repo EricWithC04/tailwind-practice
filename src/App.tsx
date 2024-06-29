2a55426c8f1af663921e74a1c70081f9f025f949
EricWithC04 <negativegame117@gmail.com>
2024-06-28 22:15:59 -0300

    feat: components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import './App.css'
+import styles from './styles.ts'
 
 function App() {
   return (
     <div className='bg-primary text-white w-full overflow-hidden'>
-      <h1>Tailwind</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+
     </div>
   )
 }
