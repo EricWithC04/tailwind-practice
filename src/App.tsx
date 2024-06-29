54aa8dcea8012a60d75b7b4c1ea67aa105949f9f
EricWithC04 <negativegame117@gmail.com>
2024-06-28 22:24:58 -0300

    feat: all components in landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
 import './App.css'
 import styles from './styles.ts'
 
+import { 
+  Navbar,
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Stats,
+  Testimonials } from './components'
+
 function App() {
   return (
     <div className='bg-primary text-white w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
